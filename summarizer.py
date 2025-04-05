22fae0e77830251b96f1b48e9ff1c649b080206c
3cc1 <145737999+3cc1@users.noreply.github.com>
2025-04-05 14:21:43 -0500

    Delete summarizer.py

--- summarizer.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-summarizer_pipeline = pipeline("summarization")
-
-def summarize_text(text):
-    summary = summarizer_pipeline(text)[0]["summary_text"]
-    return summary
